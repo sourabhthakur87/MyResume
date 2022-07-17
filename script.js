a24cad5a81a084d022feb54960f96487ae7a303f
sourabhthakur87 <sourabh.thakur_cs20@gla.ac.in>
2022-07-17 13:10:05 +0530

    New Website

---
+++ script.js
@@ -0,0 +1,19 @@
+// function changebg(){
+//     var header=document.querySelector("header")
+//     console.log(header)
+// }
+
+// window.addEventListener("scroll",function(){
+//     if(window.pageYOffset > 400){
+//         var nav2=document.getElementByI("sec2");
+//         nav2.style.display="block";
+
+      
+        
+//     }else{
+//         var s=document.getElementById("d1");
+//         s.style.background="green";
+//         var nav1=document.getElementByI("sec1");
+//         nav1.style.display="none";
+//     }
+// });
