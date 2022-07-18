e326cbe171b1e47c1c42fdf68524683c2e93526e
sourabhthakur87 <sourabh.thakur_cs20@gla.ac.in>
2022-07-18 13:14:05 +0530

    Update

--- script.js
+++ script.js
@@ -1,19 +1,47 @@
-// function changebg(){
-//     var header=document.querySelector("header")
-//     console.log(header)
-// }
+var All = document.getElementById("btn1").onclick = all;
+var web = document.getElementById("btn2").onclick = web;
+var app = document.getElementById("btn3").onclick = app;
 
-// window.addEventListener("scroll",function(){
-//     if(window.pageYOffset > 400){
-//         var nav2=document.getElementByI("sec2");
-//         nav2.style.display="block";
 
-      
-        
-//     }else{
-//         var s=document.getElementById("d1");
-//         s.style.background="green";
-//         var nav1=document.getElementByI("sec1");
-//         nav1.style.display="none";
-//     }
-// });
+
+
+
+
+function web() {
+    console.log("clicked WEB");
+    document.getElementById("app1").style.display = "none";
+    document.getElementById("app2").style.display = "none";
+    document.getElementById("app3").style.display = "none";
+    document.getElementById("app4").style.display = "none";
+
+    document.getElementById("web1").style.display = "block";
+    document.getElementById("web2").style.display = "block";
+    document.getElementById("web3").style.display = "block";
+    document.getElementById("web4").style.display = "block";
+
+}
+function app() {
+    console.log("clicked APP");
+
+    document.getElementById("web1").style.display = "none";
+    document.getElementById("web2").style.display = "none";
+    document.getElementById("web3").style.display = "none";
+    document.getElementById("web4").style.display = "none";
+
+
+    document.getElementById("app1").style.display = "block";
+    document.getElementById("app2").style.display = "block";
+    document.getElementById("app3").style.display = "block";
+    document.getElementById("app4").style.display = "block";
+}
+
+
+
+function all() {
+    console.log("clicked ALL");
+    window.location.reload();
+
+
+
+
+}
